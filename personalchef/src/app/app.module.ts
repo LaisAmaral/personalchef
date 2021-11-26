39039783fa833c063af7db3421ed75d9b9f25cdd
Pedro Gabriel <pedrogabriel12423@gmail.com>
2021-11-26 18:20:52 -0300

    Inserindo as primeiras telas

--- personalchef/src/app/app.module.ts
+++ personalchef/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from "./shared/login/login.component";
+import {Routes} from "@angular/router";
 
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  //{ path: 'signup', component: SignupComponent },
+  //{ path: 'master', component: MasterComponent },
+  //{ path: 'home', component: HomeComponent },
+  //{ path: 'reports', component: ReportsComponent },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
